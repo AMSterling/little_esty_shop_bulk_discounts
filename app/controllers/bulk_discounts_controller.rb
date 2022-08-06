68ed11c9ceebb1517d9d9413a38de036f54845aa
Anna Marie Sterling <sterling.am@gmail.com>
2022-08-05 22:22:28 -0600

    Add edit bulk discount feature

--- app/controllers/bulk_discounts_controller.rb
+++ app/controllers/bulk_discounts_controller.rb
@@ -17,9 +17,25 @@ class BulkDiscountsController < ApplicationController
       if bulk_discount.save
         redirect_to [@merchant, :bulk_discounts]
       else
-        flash[:notice] = "Error"
+        flash.notice = "Error"
         render :new
       end
+  end
+
+  def edit
+    @bulk_discount = BulkDiscount.find(params[:id])
+  end
+
+  def update
+    discount = BulkDiscount.find(params[:id])
+    discount.update(bulk_discount_params)
+    if discount.save
+      flash.notice = "Succesfully Updated Discount!"
+      redirect_to [@merchant, discount]
+    else
+      flash.notice = "All fields must be completed, get your act together."
+      redirect_to edit_merchant_item_path(@merchant, discount)
+    end
   end
 
   def destroy
