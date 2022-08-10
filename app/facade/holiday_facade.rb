39c3a016206c3083cf7c1633046b2a5b4b6b4122
Anna Marie Sterling <sterling.am@gmail.com>
2022-08-09 18:20:24 -0600

    Upcoming holidays API

---
+++ app/facade/holiday_facade.rb
@@ -0,0 +1,15 @@
+# require 'httparty'
+# require './poros/holiday'
+# require './services/holiday_service'
+#
+# class HolidayFacade
+#   def holidays
+#     holidays[0..2].map do |data|
+#       Holiday.new(data)
+#     end
+#   end
+#
+#   def service
+#     HolidayService.new
+#   end
+# end
